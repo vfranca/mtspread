e1cce319479d584989da131c53e5b1cf0b9c0e2e
Valmir Franca <vfranca3@gmail.com>
2020-02-13 16:54:39 -0300

    Colocadas os spreads em modulo; renomeados entes

--- tests/test_spread.py
+++ tests/test_spread.py
@@ -7,20 +7,20 @@ spreads = [2.50, 4.50, 1.00]
 @mock.patch("mtspread.spread.mql5")
 def test_pega_precos_do_ativo_em_um_periodo(mql5):
     mql5.CopyClose.return_value = [46.69, 46.7, 46.6]
-    assert spread.precos("ccmk20", 3) == [46.69, 46.70, 46.60]
+    assert spread.serie_precos("ccmk20", 3) == [46.69, 46.70, 46.60]
 
 
-def test_obtem_lista_de_spreads_entre_dois_precos():
-    prices1 = [1.00, 2.00, 3.00]
-    prices2 = [4.50, 5.50, 6.50]
-    assert spread.spreads(prices1, prices2) == [3.5, 3.5, 3.5]
+def test_obtem_serie_de_spreads_entre_duas_series():
+    serie1 = [1.00, 2.00, 3.00]
+    serie2 = [4.50, 5.50, 6.50]
+    assert spread.serie_spreads(serie1, serie2) == [3.5, 3.5, 3.5]
 
 
-def test_calcula_diferenca_media_de_precos():
-    assert spread.diferenca_media(spreads) == 2.67
+def test_retorna_media_de_uma_serie():
+    assert spread.media(spreads) == 2.67
 
 
-def test_calcula_o_desvio_padrao_da_serie():
+def test_retorna_o_desvio_padrao_de_uma_serie():
     assert spread.desvio_padrao(spreads) == 1.43
 
 
